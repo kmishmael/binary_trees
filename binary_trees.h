9435fd4324f1290ba7a313ecb0764aeebd5ec242
kmishmael <kmishmael@gmail.com>
2023-09-01 21:52:22 +0300

    rotate left

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -53,10 +56,10 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
-avt_t *avl_insert(avl_t **tree, int value);
-avt_t *array_to_avl(int *array, size_t size);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
-avt_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary-tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
