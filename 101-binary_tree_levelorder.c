583df18830740774c84dc399ba17885d3102547f
kmishmael <kmishmael@gmail.com>
2023-09-01 22:09:20 +0300

    level order transversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
 /**
  * binary_tree_levelorder - traverst a binary tree using level-order traverse
  * @tree: tree to traverse
